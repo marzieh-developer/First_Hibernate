c79c65009a54a9ee09ef53cfc02e32f7bfc33d52
marzieh ghassemi <gh.mgh89@gmail.com>
2019-07-24 08:47:24 +0430

    first commit first project

---
+++ src/main/java/com/RestHibeCrudr/Employee.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.RestHibeCrudr;
+
+/**
+ * @author macbook
+ *
+ */
+public class Employee {
+	private int id;
+	private String name;
+	private String family;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFamily() {
+		return family;
+	}
+	public void setFamily(String family) {
+		this.family = family;
+	}
+
+}
